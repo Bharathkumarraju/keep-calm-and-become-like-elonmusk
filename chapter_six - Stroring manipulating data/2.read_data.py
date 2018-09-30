c06601f4b293a7a152f1188bf7993fd0c90436df
Bharathkumar Dasaraju <bharath.dasaraju@delivion.de>
2018-09-30 18:29:17 +0200

    Storing the webapp logs with pythons with statement

--- chapter_six - Stroring manipulating data/2.read_data.py
+++ chapter_six - Stroring manipulating data/2.read_data.py
@@ -6,4 +6,11 @@ for i in tasks:
 tasks.close()
 
 
+# Most programmers use 'with' statement while doing file operations that is open read/write close
+# the beauty of 'with' statement is that it is smart enough to close files automatically when with block ends
+# more importantly 'With' statement conforms to a coding convention built into python called the context management protocol
+
+with open('todos.txt') as lines:
+    for i in lines:
+        print('\t'*18, i, end='')
 
---
+++ chapter_six - Stroring manipulating data/3.webapp_logs_capture.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from wordsearch import search4letters
+
+vayuputhraapp = Flask(__name__)
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('webappsearch.log','a') as log:
+        print(req, res, file=log)
+
+@vayuputhraapp.route('/search4', methods=['POST'])
+def search_do() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    log_request(request, results)
+    return render_template('results.html',
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results,)
+@vayuputhraapp.route('/')
+@vayuputhraapp.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Welcome to the search4letters on the web!')
+
+if __name__ == '__main__':
+    vayuputhraapp.run(debug=True)
+
+
