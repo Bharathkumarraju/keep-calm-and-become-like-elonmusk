181b7dc08e588bc6ca7b419faa583069accab51b
Bharathkumar Dasaraju <bharath.dasaraju@delivion.de>
2018-09-30 16:55:15 +0200

    Deploying a web app with setting the dunder name

---
+++ chapter_five - Getting Real/4.word_search_webapp_multiple_urls.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from wordsearch import search4letters
+
+
+vayuputhraapp = Flask(__name__)
+
+'''
+@vayuputhraapp.route('/')
+def hello() -> '302':
+    return redirect('/entry')
+'''
+
+@vayuputhraapp.route('/search4', methods=['POST'])
+def search_do() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html',
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results,)
+@vayuputhraapp.route('/')
+@vayuputhraapp.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Welcome to the search4letters on the web!')
+
+vayuputhraapp.run(debug=True)
+
---
+++ chapter_five - Getting Real/dunder.py
@@ -0,0 +1,9 @@
+def spaces(num):
+    for i in range(num):
+        print("")
+spaces(5)
+print("We start of in the value of dunder name is", __name__)
+spaces(5)
+if __name__ == '__main__':
+    print("We ended of in the value of dunder name is", __name__)
+
