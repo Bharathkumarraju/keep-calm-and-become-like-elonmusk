5a3d9efd94e9753d6d02700ddd56c50fe7889d0d
Bharathkumar Dasaraju <bharath.dasaraju@delivion.de>
2018-10-15 07:13:24 +0200

    Pythons exception handling how it works

---
+++ Chaper_Eleven - Exception Handling/1.Database-api-context-management.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, escape
+from wordsearch import search4letters
+from dbcm import UseDataBase
+
+# If your class defines dunder "enter" and dunder "exit" it's a context manager
+
+vayuputhraapp = Flask(__name__)
+
+vayuputhraapp.config['dbconfig'] = {
+    'host': '127.0.0.1',
+    'user': 'wordsearch',
+    'password': 'wordsearchpassword',
+    'database': 'wordsearchlogDB' }
+def log_request(req: 'flask_request', res: str) -> None:
+    """Log details of web requests and results"""
+    with UseDataBase(vayuputhraapp.config['dbconfig']) as cursor:
+        _SQL = """insert into log(phrase, letters, ip, browser_string, results) values(%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL,(
+            req.form['phrase'],
+            req.form['letters'],
+            req.remote_addr,
+            req.user_agent.browser,
+            res))
+
+@vayuputhraapp.route('/search4', methods=['POST'])
+def search_do() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    log_request(request, results)
+    return render_template('results.html',
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results,)
+@vayuputhraapp.route('/')
+@vayuputhraapp.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Welcome to the search4letters on the web!')
+
+@vayuputhraapp.route('/viewlog')
+def view_the_log() -> 'html':
+   with UseDataBase(vayuputhraapp.config['dbconfig']) as cursor:
+       _SQL1 = """select phrase, letters, ip, browser_string, results from log"""
+       cursor.execute(_SQL1)
+       contents = cursor.fetchall()
+   titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+   return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents)
+
+if __name__ == '__main__':
+    vayuputhraapp.run(debug=True)
+
+
