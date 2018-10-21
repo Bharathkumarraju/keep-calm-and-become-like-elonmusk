4590e528de827b855931b58cccb6aa49560a5b12
Bharathkumar Dasaraju <bharath.dasaraju@delivion.de>
2018-10-21 09:58:15 +0200

    how exc_info() method of sys module works with exceptions

--- Chapter_Eleven - Exception Handling/3.exception_with_exc_info.py
+++ Chapter_Eleven - Exception Handling/3.exception_with_exc_info.py
@@ -1,7 +1,24 @@
 import sys
+"""
+A standard library comes with a module called sys that provides access to the interpreters internals
+one such function is exc_info()
+exc_info() returns three values tuple
+1. First value indicates the exceptions type
+2. Second details the exceptions value
+3. Third contains the traceback object that provides the access to the traceback message
+"""
+
 sys.exc_info()
 print("")
 print(sys.exc_info())
 
+
+
+try:
+    1/0
+except:
+    err = sys.exc_info()
+    for e in err:
+        print(e)
 
 
