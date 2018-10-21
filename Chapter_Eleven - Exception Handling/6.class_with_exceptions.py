ed68de5c832664279893fe5ba0f4e48d124ced32
Bharathkumar Dasaraju <bharath.dasaraju@delivion.de>
2018-10-21 14:08:25 +0200

    ConnectionError exception with inheriting the Exception class

--- Chapter_Eleven - Exception Handling/6.class_with_exceptions.py
+++ Chapter_Eleven - Exception Handling/6.class_with_exceptions.py
@@ -1,3 +1,6 @@
+# ConnectionError inherits from the pythons built-in Exception class.
+# Means that all of the attributes and behaviors of Exception are available in ConnectionError too.
+
 class ConnectionError(Exception):
     pass
 raise ConnectionError('Can not connect it is time to panic')
