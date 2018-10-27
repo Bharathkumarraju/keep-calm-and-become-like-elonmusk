b8d2373b99c9103d317cc60cd997d12f9f1278cf
Bharathkumar Dasaraju <bharath.dasaraju@delivion.de>
2018-10-27 05:06:36 +0200

    list_old=[2,3,4] list_new=list(map(lambda x: x**3, list_old))

--- pythons_lambda/lambda_test.py
+++ pythons_lambda/lambda_test.py
@@ -3,3 +3,30 @@ def num_cubed(num):
 
 print("")
 print(num_cubed(3))
+print("")
+print("")
+
+list_normal = [2, 3, 4, 5, 6, 7, 8, 9]
+list_cubed = []
+
+for num in list_normal:
+    list_cubed.append(num_cubed(num))
+    print(list_cubed)
+
+print("")
+print("")
+#above 3 lines wrapped in single line as below
+
+hanuman_list_cubed = [num_cubed(x) for x in list_normal]
+
+print(hanuman_list_cubed)
+
+
+# We have list_normal and list_cubed and hanuman_list_cubed, let's do magic with lambda
+print("")
+print("")
+
+list_normal_new = [2, 3, 4, 5, 6, 7, 8, 9]
+list_cubed_new = list(map(lambda x: x**3, list_normal_new))
+
+print(list_cubed_new)
