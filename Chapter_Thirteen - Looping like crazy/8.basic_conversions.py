9eeee0c06ac71ee6b374b74334186df5896f6c25
Bharathkumar Dasaraju <bharath.dasaraju@delivion.de>
2018-11-05 06:32:12 +0100

    convert dictionary of items into title case and time to AM/PM format

--- Chapter_Thirteen - Looping like crazy/8.basic_conversions.py
+++ Chapter_Thirteen - Looping like crazy/8.basic_conversions.py
@@ -1,3 +1,6 @@
+import pprint
+from datetime import datetime
+
 s = "This is my first python's coversion program"
 print("")
 print(s)
@@ -7,12 +10,32 @@ print("")
 
 # how to use datetime module with string format specifiers
 
-from datetime import datetime
+
 
 def convert2ampm(time24: str) -> str:
-    return print(datetime.strptime(time24, '%H:%M').strftime('%I:%M%p'))
+    return datetime.strptime(time24, '%H:%M').strftime('%I:%M%p')
 
 convert2ampm('19:00')
-
 convert2ampm('21:00')
 
+print("")
+print("")
+
+try:
+    with open('hanuman.csv') as data:
+        data.readline()
+        flights = {}
+        for i in data:
+            k, v = i.strip().split(',')
+            flights[k] = v
+    pprint.pprint(flights)
+    print("")
+    flights2 = {}
+    for k, v in flights.items():
+        flights2[convert2ampm(k)] = v.title()
+    pprint.pprint(flights2)
+except Exception as err:
+    print("Error is %s" % err)
+
+print("")
+print("")
