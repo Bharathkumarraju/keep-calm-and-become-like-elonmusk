7a8bfd81b6de886f4a80b75a155af29ea9731f56
Bharathkumar Dasaraju <bharath.dasaraju@delivion.de>
2018-11-15 13:01:05 +0530

    Add list comprehension ang Generators with the help of requests python module

--- Chapter_Thirteen - Looping like crazy/14.process_urls_listcomps_generators.py
+++ Chapter_Thirteen - Looping like crazy/14.process_urls_listcomps_generators.py
@@ -11,3 +11,16 @@ print("\n"*2)
 print(output.content)
 print("")
 print("\t"*2, output.status_code)
+print("\n"*3)
+
+urls = ('http://google.com', 'http://twitter.com')
+
+for url in urls:
+    print(url)
+
+print("")
+
+for resp in [ requests.get(urlz) for urlz in urls]:
+     print(len(resp.content), '-->', resp.status_code, '-->', resp.url)
+
+print("\n"*3)
